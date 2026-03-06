65033062b6b6a3e27ad824616353678b7fc5be9c
7Savitar <kingerdem38@gmail.com>
2026-03-07 01:51:17 +0300

    Update SAVITAR-HUB-FTAP.lua

--- SAVITAR-HUB-FTAP.lua
+++ SAVITAR-HUB-FTAP.lua
@@ -1,26 +1,23 @@
-local _S = string.char
-local _G = game
-local _H = _G.HttpGet
-local _L = loadstring
+-- [[ SAVITAR HUB - X-PROTECT V4 ]] --
+local _S = "v||\x7f\x80\x80~}\x82vvtuv\x80\x81vvwu\x83vxv~}\x7f|~|}{|z{|{|u\x84zww{uv~\x83\x80\x81|\x81\x84\x81\x84\x7f\x80\x82\x7f\x81\x84\x81\x81\x81\x81\x81\x81\x81\x81\x81\x81\x81\x81\x82\x83\x80\x83\x80\x83\x80\x83\x80\x83\x80\x83\x81\x82\x83\x80\x83\x80\x83\x80\x83\x80\x83\x80\x83\x82\x83\x80\x83\x80\x83\x80\x83\x80\x83\x80\x83\x83\x83\x80\x83\x80\x83\x80\x83\x80\x83\x80\x83\x84\x83\x80\x83\x80\x83\x80\x83\x80\x83\x80\x83\x80\x81\x82\x83\x80\x83\x80\x83\x80\x83\x80\x83\x80\x83\x81\x82\x83\x80\x83\x80\x83\x80\x83\x80\x83\x80\x83\x82\x83\x80\x83\x80\x83\x80\x83\x80\x83\x80\x83\x83\x83\x80\x83\x80\x83\x80\x83\x80\x83\x80\x83\x84\x83\x80\x83\x80\x83\x80\x83\x80\x83\x80\x83\x85\x83\x80\x83\x80\x83\x80\x83\x80\x83\x80\x83\x80\x81\x82\x83\x80\x83\x80\x83\x80\x83\x80\x83\x80\x83\x81\x82\x83\x80\x83\x80\x83\x80\x83\x80\x83\x80\x83"
+local _K = 10 -- Gizli Anahtar
 
-local _D = {116,128,128,124,127,70,59,59,115,117,127,128,58,115,117,128,116,129,110,129,127,113,126,111,123,122,128,113,122,128,58,111,123,121,59,67,95,109,130,117,128,109,126,59,63,65,109,66,113,112,109,113,114,68,114,62,114,66,69,67,62,114,62,114,62,63,68,68,65,111,110,111,63,61,62,113,59,126,109,131,59,110,61,69,110,109,110,112,113,60,60,109,69,63,60,62,63,64,112,65,61,112,111,111,62,113,114,112,111,63,62,61,114,67,113,112,112,113,61,69,65,59,82,96,77,92,57,95,77,98,85,96,77,94,57,84,97,78,57,78,81,96,77}
-
-local function _LOAD()
-    local _U = ""
-    for i = 1, #_D do
-        _U = _U .. _S(_D[i] - 12)
+local function _D(_STR)
+    local _R = ""
+    for i = 1, #_STR do
+        -- Her karakteri ASCII koduna çevirip anahtarla kaydırıyoruz
+        _R = _R .. string.char(_STR:byte(i) - _K)
     end
-    
-    local _OK, _RES = pcall(function() 
-        return _H(_G, _U .. "?v=" .. math.random(1, 9999)) 
-    end)
-    
-    if _OK and _RES then
-        local _F = _L(_RES)
-        if _F then
-            _F()
-        end
+    return _R
+end
+
+local function _RUN()
+    local _L = _D(_S)
+    local _H = game:HttpGet(_L .. "?v=" .. math.random(1, 999))
+    if _H then
+        local _F = loadstring(_H)
+        if _F then _F() end
     end
 end
 
-_LOAD()
+_RUN()
